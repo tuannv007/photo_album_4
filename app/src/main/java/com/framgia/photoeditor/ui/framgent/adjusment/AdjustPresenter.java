067dee756e4235b3c8312294c1de963f63d86732
tuannv007 <tuan.dev.java@gmail.com>
2017-01-13 10:14:09 +0700

    fix_image_to_black_white

---
+++ app/src/main/java/com/framgia/photoeditor/ui/framgent/adjusment/AdjustPresenter.java
@@ -0,0 +1,47 @@
+package com.framgia.photoeditor.ui.framgent.adjusment;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+import com.framgia.photoeditor.util.Util;
+
+/**
+ * Created by tuanbg on 1/13/17.
+ */
+public class AdjustPresenter implements AdjusmentContract.Presenter {
+    private AdjusmentContract.View mView;
+
+    public AdjustPresenter(AdjusmentContract.View view) {
+        mView = view;
+        mView.start();
+    }
+
+    @Override
+    public void convertImgBlackWhite(Bitmap bitmap) {
+        new ImgToBlackWhiteAsync().execute(bitmap);
+    }
+
+    @Override
+    public boolean saveImage(Bitmap bitmap) {
+        return false;
+        // TODO: 1/13/17  
+    }
+
+    @Override
+    public void handleSave(Bitmap bitmap) {
+        // TODO: 1/13/17
+    }
+
+    public class ImgToBlackWhiteAsync extends AsyncTask<Bitmap, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(Bitmap... params) {
+            return Util.convertImageToBlackWhite(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            mView.updateImgBlackWhite(bitmap);
+        }
+    }
+}
